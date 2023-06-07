4d3f67269b6c91abd88b5bf8bd66bdae70eba6c4
Gabriel Levy <levygabriel11@gmail.com>
2023-06-07 08:01:44 -0300

    Criado a classe Conta

---
+++ src/main/java/br/edu/ifmg/bsi/prog3/Conta.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mavenproject2;
+
+/**
+ *
+ * @author aluno
+ */
+public class Conta {
+    private int numero;
+    private double valor;
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+    
+    public Conta(int numero, double valor) {
+        this.numero = numero;
+        this.valor = valor;
+    }
+
+}
